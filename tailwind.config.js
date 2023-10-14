6ac58afd6337c7e788336800d389d55bf6f682a4
Salomao <salomao.machado.75@gmail.com>
2023-10-14 18:12:26 -0300

    alterando cores e terminando projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,23 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'fundo': '#126945',
+      'cinza': '#a8a29e',
+      'cinza2': '#94a3b8',
+      'cinza3': '#334155',
+      'jade3': '#1acd81',
+      'jade2': '#81f4c3',
+      'jade1': '#b8fadd',
+      'botoes1': '#bacbff',
+      'botoes2': '#f93a3a',
+      'botoes3': '#c21313',
+
+    },
+  },
 }
 
